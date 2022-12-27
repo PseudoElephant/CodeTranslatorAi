2071d6b0b4d8ae05bbc9bcb5cee010e731786fdf
Stephan Guingor <stephan.guingor04@gmail.com>
2022-12-26 18:23:43 -0600

    Setup planetscale and prisma

--- backend/src/functions/checkout-session/index.ts
+++ backend/src/functions/checkout-session/index.ts
@@ -1,11 +1,22 @@
 // src/functions/cars/index.ts
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 export const handler = async (_event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
+       const r = await prisma.package.create({
+                data: {
+                    name: 'test',
+                    priceUsd: 100,
+                    numTranslations: 100,
+                }   
+        })
+
         const response = {
             statusCode: 200,
-            body: _event.path,
+            body: JSON.stringify(r),
         };
         return response;
     } catch (err) {
